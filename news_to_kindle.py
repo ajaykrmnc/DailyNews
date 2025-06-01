f3b6484ac7a67d17317f9e33ed4ae12ece03fd9f
ajaykrmnc <ajaykg6917@gmail.com>
2025-06-01 09:33:48 +0530

    error check

--- news_to_kindle.py
+++ news_to_kindle.py
@@ -159,6 +159,8 @@ def send_to_kindle():
         result = subprocess.run([
             "/usr/bin/calibre-smtp",
             "--port", "587",
+            "--attachement", EPUB_FILE,
+            "--subject", f"Prelims Pointers - {DATE}",
             "--encryption-method", "TLS",
             "--relay", SMTP_SERVER,
             "--username", SMTP_USERNAME,
