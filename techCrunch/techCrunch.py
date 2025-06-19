d762491a79f618b1ea23950abb4cf887ae667805
ajaykrmnc <ajaykg6917@gmail.com>
2025-06-19 11:51:20 +0530

    techCrunch cloudsraper commit

--- techCrunch/techCrunch.py
+++ techCrunch/techCrunch.py
@@ -7,19 +7,11 @@ import cloudscraper
 import os
 load_dotenv()
 from readabilipy import simple_json_from_html_string
-from playwright.sync_api import sync_playwright
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from extractFunction.parseFunction import saveImages, convert_file_to_epub, send_to_kindle
 # Parse the RSS feed
 
-headers = {
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1"
-}
 
 def techCrunch():
     feed_url = "https://techcrunch.com/feed/"
@@ -58,10 +50,43 @@ def techCrunch():
             if div:
                 div.decompose();
         for img in soup.find_all("img"):
-            img_src = img.get("src");
-            img_src_last = img_src.split("/")[-1];
-            if(img_src_last == "analytics"):
-                img.decompose();
+            img_src = img.get("src")
+            img_src_last = img_src.split("/")[-1] if img_src else ""
+            if img_src_last == "analytics":
+                img.decompose()
+        for svg in soup.find_all("svg"):
+            if svg:
+                svg.decompose()
+        for a in soup.find_all("a"):
+            if a:
+                a.unwrap()
+
+        img_path = f"techCrunch/images"
+        img_full_path = os.path.abspath(img_path);
+        saveImages(soup, img_full_path)
+        # Append the processed HTML to the article string
+        article += str(soup)
+    cnt = 0;
+    feed = feedparser.parse(feed_url)
+    for entry in feed.entries:
+        # Truncate the file before writing (overwrite mode)
+        cnt += 1;
+        if(cnt >= 5):
+            break;
+        # fetch the content of the article
+
+        
+        scraper = cloudscraper.create_scraper()
+        response = scraper.get(entry.link, timeout=20)
+        
+            
+        
+        
+        # entry.content is usually a list of dicts with 'value' as the HTML content
+        # print(html_string)
+        article_data = simple_json_from_html_string(response.text, use_readability=True)
+        html_content = f"<h2>{article_data['title']}</h2>{article_data['content']}"
+        soup = BeautifulSoup(html_content, "html.parser")
         for svg in soup.find_all("svg"):
             if svg:
                 svg.decompose()
@@ -91,4 +116,4 @@ def techCrunch():
     convert_file_to_epub(yourStory, epub_file, f"techCrunch/techCrunch.png")
     send_to_kindle(epub_file);
  
-techCrunch();
+# techCrunch();
