c5aa3adb921b5912ecd205be0e130690c90c2f11
ajaykrmnc <ajaykg6917@gmail.com>
2025-06-24 15:16:20 +0530

    timgap update

--- ncert/onebyone.py
+++ ncert/onebyone.py
@@ -8,7 +8,8 @@ def sendgeography():
     today = datetime.today().strftime("%Y-%m-%d");
     start = "2025-06-20";
     timegap = (datetime.strptime(today, "%Y-%m-%d") - datetime.strptime(start, "%Y-%m-%d")).days;
-    epub_file = f"ncert/GEOGRAPHY/File_{timegap}.epub";
+    timegap = int(timegap / 3)
+    epub_file = f"ncert/GEOGRAPHY/File_{timegap}.epub"
     if epub_file:
         send_to_kindle(EPUB_FILE=epub_file)
 
